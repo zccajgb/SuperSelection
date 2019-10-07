390636ebfa2243dcd7efb3a2986c195fa3717888
Joe Buckley <josephgbuckley@gmail.com>
2019-10-07 20:37:41 +0100

    added jwt

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthenticationService } from '../_services/authentication.service';
+import { User } from 'src/models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,6 +12,7 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  currentUser: User;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +20,20 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
 
+    logout() {
+      this.authenticationService.logout();
+      this.router.navigate(['/login']);
+    }
+
+    isAuthenticated(): boolean {
+      return this.currentUser != null;
+    }
 }
